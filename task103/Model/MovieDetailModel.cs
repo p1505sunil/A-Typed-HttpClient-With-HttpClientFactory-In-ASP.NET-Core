bdc9fdf6fa3fcd33d5974dddfb4bd8ab4f9bc8a9
Venkatesh <vkalva45@gmail.com>
2019-10-25 00:52:58 -0500

    create a View Model class to hold the data from the OMDB API. add a class named MovieDetailModel.

---
+++ task103/Model/MovieDetailModel.cs
@@ -0,0 +1,13 @@
+namespace MovieFinder.Models
+{
+    public class MovieDetailModel
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+        public string Actors { get; set; }
+        public string IMDBRating { get; set; }
+        public string PosterImage { get; set; }
+        public string Plot { get; set; }
+    }
+}
